f1e14994cc55c1f1e43e6c15d76831095c3aeb96
Venu <venukb6@gmail.com>
2024-05-09 17:00:14 +0530

    fetch saved data(details) from backend and display it on frontend

--- Expense-tracker-1/app.js
+++ Expense-tracker-1/app.js
@@ -1,9 +1,42 @@
+// window.addEventListener("DOMContentLoaded", ()=>{
+//     axios
+//     .get("https://crudcrud.com/api/fb5925419edb47e895abb7a9d7220ce3/appointmentDetails")
+//     .then((response)=>{
+//         console.log(response)
+//         for(let i=0; i<response.data.length; i++){
+//             displayUsersOnScreen(response.data[i])
+//         }
+//     })
+// })
+
+// function displayAllUsersOnScreen(details){
+//     const newLi = document.createElement('li')
+//     newLi.innerHTML = `${details.amtV} - ${details.desV} - ${details.catV} <button class="dlt">Delete Expense</button>  <button class="edt">Edit Expense</button>`
+//     newLi.className = "list-group-item"
+    
+//     ul.appendChild(newLi)
+// }
+
+
 document.addEventListener('DOMContentLoaded',()=>{
     const form = document.querySelector('form')
     const amount = document.getElementById('amount')
     const desc = document.getElementById('description')
     const category = document.getElementById('category')
     const ul = document.querySelector('.expenseUl')
+
+
+    axios
+    .get("https://crudcrud.com/api/fb5925419edb47e895abb7a9d7220ce3/appointmentDetails")
+    .then((response)=>{
+        for(let i=0; i<response.data.length; i++){
+            displayUserOnScreen(response.data[i])
+        }
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+
 
     function displayUserOnScreen(details){
         const newLi = document.createElement('li')
@@ -29,7 +62,7 @@ document.addEventListener('DOMContentLoaded',()=>{
         .post("https://crudcrud.com/api/fb5925419edb47e895abb7a9d7220ce3/appointmentDetails",
             details
         )
-        .then((result) => displayUserOnScreen(result.Data))
+        .then((result) => displayUserOnScreen(result.data))
         .catch((err)=>{
             console.log(err)
         })
